9feb4e57c9279692f206f0442d130f23f8f5349d
jadhavgajanan <gajananjadhav1020@gmail.com>
2022-08-18 01:30:17 +0530

    PBA-3:create the Entity class with several field

---
+++ src/main/java/com/BikkadIT/model/Contact.java
@@ -0,0 +1,94 @@
+package com.BikkadIT.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CONTACT_DTLS")
+public class Contact {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer contactId;
+	
+	private String contactName;
+	
+	private String contactNumber;
+	
+	private String contactEmail;
+	
+	private Character activeSwitch;
+	
+	private LocalDate createDate;
+	
+	private  LocalDate updateDate;
+
+	public Integer getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public Character getActiveSwitch() {
+		return activeSwitch;
+	}
+
+	public void setActiveSwitch(Character activeSwitch) {
+		this.activeSwitch = activeSwitch;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDate getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(LocalDate updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [contactId=" + contactId + ", contactName=" + contactName + ", contactNumber=" + contactNumber
+				+ ", contactEmail=" + contactEmail + ", activeSwitch=" + activeSwitch + ", createDate=" + createDate
+				+ ", updateDate=" + updateDate + "]";
+	}
+	
+	
+}
